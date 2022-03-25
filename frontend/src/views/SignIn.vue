e636cf537ed89cd4a530cb5919a971ea46a2c8d3
sunshine4437 <sunshine4437@gmail.com>
2022-03-25 15:37:04 +0900

    go

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -326,7 +326,7 @@ export default {
         },
         loginWithKakao() {
             if (window.Kakao.isInitialized() == false) {
-                window.Kakao.init('255b187f87731368f5e47c3310b3cf02');
+                window.Kakao.init('cdfb2db423a8934be59346987db27ae2');
             }
             window.Kakao.Auth.login({
                 success: this.kakaoLoginCheck(),
@@ -426,7 +426,7 @@ export default {
         else
             this.setPath('/')
 
-        const naver_id_login = new window.naver_id_login("INmTkpuK5mPhbhHfYG_Q", "http://118.67.128.121:8085/authentication/signIn/naver");
+        const naver_id_login = new window.naver_id_login("7ulr5gz1NWagQvutvDBP", "http://27.96.131.216:8085/authentication/signIn/naver");
         let link = document.location.href;
         if (link.indexOf('access_token') != -1) {
             this.naverLogin(naver_id_login.getAccessToken());
